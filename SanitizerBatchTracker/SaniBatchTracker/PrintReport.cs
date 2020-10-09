a339d1cde07a89af7acdc6c2b8c20adac4cd25bd
Keith Brooks <keithb@millmats.com>
2020-10-09 13:54:02 -0400

    Palet Label Size change to 4x2

--- SanitizerBatchTracker/SaniBatchTracker/PrintReport.cs
+++ SanitizerBatchTracker/SaniBatchTracker/PrintReport.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28e6dde9c410c76e5498a42fcb5f4be98f329583165456c61616811601ba875d
-size 4692
+oid sha256:9e7182c310690028e7bf8a0fa2dbae91b00437bafffdf869fa9c46193dfcba53
+size 4700
--- SanitizerBatchTracker/SaniBatchTracker/SaniBatchTracker.cs
+++ SanitizerBatchTracker/SaniBatchTracker/SaniBatchTracker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:469e49665e9ccb9a94135972d47ccf5dd4ebb9be42d3df37d8ad6788482b6c83
-size 25579
+oid sha256:04c6ba65889669bfedafe0a8698ea0cd1abeee3d0f1ae0ab779763b29fe77875
+size 26456
